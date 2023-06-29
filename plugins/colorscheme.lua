f8362c9ea8e1905ed879a5000d34183ba21add93
Fengzikai <FengisZZZ@outlook.com>
2023-06-29 17:19:05 +0800

    add colorscheme.lua

---
+++ plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "olimorris/onedarkpro.nvim",
+        opts = {
+            options = {
+                highlight_inactive_windows = true,
+                transparency = true,
+                terminal_colors = true,
+            },
+        },
+    },
+}
